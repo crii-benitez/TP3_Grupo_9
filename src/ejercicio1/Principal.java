8027c24a66ae1a4594b4d2680628b339f9ff543d
Pavi <gustavopavichevich@gmail.com>
2021-04-27 02:32:01 -0300

    Persona.java: Cambié el else del punto c. Le cambie el true del append a false.

--- src/ejercicio1/Principal.java
+++ src/ejercicio1/Principal.java
@@ -1,11 +1,8 @@
 package ejercicio1;
 
-
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
-import java.util.List;
 import java.util.TreeSet;
 
 import ejercicio1.Archivo;
@@ -23,119 +20,42 @@ public class Principal {
 		// función verificarDniInvalido para validar que el dni sea correcto)
 
 		TreeSet<Persona> listaPersonas = new TreeSet<Persona>();
-		
-		
+
 		ArrayList<String> listaArchivo = new ArrayList<String>();
 		listaArchivo = Archivo.leerArchivo("./archivos/Personas.txt");
 		Iterator<String> it = listaArchivo.iterator();
 		while (it.hasNext()) {
 			Persona persona = new Persona();
-			
-			String txt = it.next();
-			//System.out.println(txt);
-			
-			String[] parts = txt.split("-");
 
-			
+			String txt = it.next();
+			String[] parts = txt.split("-");
 			String valor = "";
 			String valor1 = "";
 			String valor2 = "";
 			valor = parts[0];
 			valor1 = parts[1];
 			valor2 = parts[2];
-			
-			System.out.println(valor + " "+ valor1 + " " + valor2);
-			
-			
-			
+
+			System.out.println(valor + " " + valor1 + " " + valor2);
+
 			persona.setNombre(valor);
 			persona.setApellido(valor1);
-		    persona.setDni(valor2); 
-		    try {
-		    	Validaciones.verificarDniInvalido(valor2);
-		    	listaPersonas.add(persona);
-		    	System.out.println("grabo");
-		    	//punto c
-				if (Archivo.existeArchivo("./archivos/Resultado.txt")==false)
-				{
-					Archivo.escribeArchivo("./archivos/Resultado.txt",true,valor + " "+ valor1 + " " + valor2);
+			persona.setDni(valor2);
+			try {
+				Validaciones.verificarDniInvalido(valor2);
+				listaPersonas.add(persona);
+				System.out.println("grabo");
+				// punto c
+				if (Archivo.existeArchivo("./archivos/Resultado.txt") == false) {
+					Archivo.escribeArchivo("./archivos/Resultado.txt", true, valor + " " + valor1 + " " + valor2);
+				} else {
+					Archivo.escribeArchivo("./archivos/Resultado.txt", false, valor + " " + valor1 + " " + valor2);
 				}
-				else
-				{
-					Archivo.escribeArchivo("./archivos/Resultado.txt",true,valor + " "+ valor1 + " " + valor2);
-				}
-		    }
-		    catch (Exception e) {
-		    	
-		    	
-		    	System.out.println("no grabo");
+			} catch (Exception e) {
+				System.out.println("no grabo");
 			}
-
-		//	listaPersonas.add(persona);
 		}
 		System.out.println("break");
-		
-		// punto B
-		// FileReader entrada;
-		// ArrayList<Persona> listaPersonas = new ArrayList<Persona>();
-		// try {
-		// //entrada = new FileReader("./archivos/Personas.txt");
-		// //BufferedReader miBuffer = new BufferedReader(entrada);
-		//
-		// String linea = "";
-		// while (linea != null) {
-		// linea = miBuffer.readLine();
-		// /*lo agrego a la lista tipo persona*/
-		// //String separador = Pattern.quote("-");
-		// String[] parts = new String[50];
-		// parts= linea.split("-");
-		// // System.out.println(parts[0]+parts[1]+parts[2]);
-		// if(Validaciones.verificarDniInvalido(parts[2]))
-		// {
-		//// int dni_ =Integer.parseInt(parts[2]);
-		//// }
-		//// if (String.valueOf(dni_).length()==7 || String.valueOf(dni_).length()==8)
-		//// {
-		//
-		// Persona p1 = new Persona();
-		// p1.setNombre(parts[0].toString());
-		// p1.setApellido(parts[1].toString());
-		// p1.setDni(Integer.parseInt(parts[2].toString()));
-		// listaPersonas.add(p1);
-		// }
-		// //System.out.println(linea);
-		// }
-		//
-		// // entrada.close();
-		//
-		//
-		//
-		//
-		// }
-		// catch (IOException e) {
-		// System.out.println(e.getMessage() + "Leo");
-		// }
-		// catch(ArrayIndexOutOfBoundsException e)
-		// {
-		// System.out.println("Se está tratando de acceder a una posición inválida");
-		// }
-		// catch(NumberFormatException e)
-		// {
-		// System.out.println("Formato Dni incorrecto");
-		// }
-		// finally
-		// {
-		// System.out.println("Listado de personas en el archivo: ");
-		//
-		// Iterator<Persona> it = listaPersonas.iterator();
-		// while (it.hasNext()) {
-		// Persona persona = it.next();
-		//// it.remove();
-		// System.out.println(persona.toString());
-		// }
-		// }
-		// miBuffer.close();
-		//
 	}
 
 }
