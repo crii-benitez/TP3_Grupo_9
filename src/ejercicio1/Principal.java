7e13c1cd40a916070bc1f492aa1c107a277b1b01
u582985 <u582985@T0002983724.telecom.arg.telecom.com.ar>
2021-04-27 03:46:21 -0300

    Personas.java limpieza de codigo y creacion de compareTo.

--- src/ejercicio1/Principal.java
+++ src/ejercicio1/Principal.java
@@ -11,20 +11,14 @@ import ejercicio1.Persona;
 public class Principal {
 
 	public static void main(String[] args) throws IOException {
-		// ejercicio B hecho por pavi
-		// B. Leer el archivo llamado Personas.txt, que está compuesto por Nombre
-		// – Apellido – Dni y pasar los datos que se encuentran en el archivo a una
-		// lista que no acepte datos duplicados, que permita ordenar los datos
-		// según el Apellido desde la A – Z y además no se agregará a la lista Dni
-		// que no cumplan el requisito de solo contener números (utilizar la
-		// función verificarDniInvalido para validar que el dni sea correcto)
 
 		TreeSet<Persona> listaPersonas = new TreeSet<Persona>();
-
 		ArrayList<String> listaArchivo = new ArrayList<String>();
+
 		listaArchivo = Archivo.leerArchivo("./archivos/Personas.txt");
 		Iterator<String> it = listaArchivo.iterator();
-		while (it.hasNext()) {
+		
+		while(it.hasNext()){
 			Persona persona = new Persona();
 
 			String txt = it.next();
@@ -36,26 +30,24 @@ public class Principal {
 			valor1 = parts[1];
 			valor2 = parts[2];
 
-			System.out.println(valor + " " + valor1 + " " + valor2);
-
 			persona.setNombre(valor);
 			persona.setApellido(valor1);
 			persona.setDni(valor2);
+			
 			try {
+				
 				Validaciones.verificarDniInvalido(valor2);
 				listaPersonas.add(persona);
-				System.out.println("grabo");
-				// punto c
+				
 				if (Archivo.existeArchivo("./archivos/Resultado.txt") == false) {
-					Archivo.escribeArchivo("./archivos/Resultado.txt", true, valor + " " + valor1 + " " + valor2);
+					Archivo.escribeArchivo("./archivos/Resultado.txt", true, valor + " " + valor1 + " " + valor2 + "\n");
 				} else {
-					Archivo.escribeArchivo("./archivos/Resultado.txt", false, valor + " " + valor1 + " " + valor2);
+					Archivo.escribeArchivo("./archivos/Resultado.txt", false, valor + " " + valor1 + " " + valor2 + "\n");
 				}
 			} catch (Exception e) {
-				System.out.println("no grabo");
+				e.printStackTrace();
 			}
-		}
-		System.out.println("break");
+			}
 	}
 
 }
